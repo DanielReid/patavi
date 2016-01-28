9a1785af93a54ae365a1ba8d1666807f0bf1f994
Connor Stroomberg <c.stroomberg@gmail.com>
2016-01-28 12:49:42 +0100

    use env var to set test url

--- service-monitoring/isItUpTest.js
+++ service-monitoring/isItUpTest.js
@@ -1,10 +1,11 @@
+var testUrl = process.env.PATAVI_TEST_URL;
 module.exports = {
 	"Is Patavi demo page up ? test": function(browser) {
 
 		var expectedText = "Congratulations! It looks like you have configured the server correctly. You can now start any number of workers and access their tasks by using WAMP.";
 
 		browser
-			.url("https://patavi.drugis.org/")
+			.url(testUrl)
 			.waitForElementVisible('body', 5000)
 			.assert.containsText("body > div:nth-child(2) > div > p", expectedText)
 			.end();
